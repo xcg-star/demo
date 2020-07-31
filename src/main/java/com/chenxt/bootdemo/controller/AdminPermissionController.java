e406f0eef24192fcc5485c4caa542a43c90d2acf
chenxt <506195407@qq.com>
2020-07-31 23:38:47 +0800

    feat: permission

---
+++ src/main/java/com/chenxt/bootdemo/controller/AdminPermissionController.java
@@ -0,0 +1,17 @@
+package com.chenxt.bootdemo.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 后台管理
+ *
+ * @date 2020/07/31
+ */
+@RestController
+@Api(tags = "后台管理权限Controller")
+@RequestMapping("/member/v1")
+public class AdminPermissionController {
+
+}
