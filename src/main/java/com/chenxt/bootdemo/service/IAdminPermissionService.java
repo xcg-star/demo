a032e712a7fd7fd768f8f75887b6774c3b07210b
chenxt <506195407@qq.com>
2020-08-04 22:00:24 +0800

    feat: admin user

--- src/main/java/com/chenxt/bootdemo/service/IAdminPermissionService.java
+++ src/main/java/com/chenxt/bootdemo/service/IAdminPermissionService.java
@@ -3,6 +3,7 @@ package com.chenxt.bootdemo.service;
 /**
  * 后台管理权限service接口
  *
+ * @author chenxt
  * @date 2020/08/03
  */
 public interface IAdminPermissionService {
---
+++ src/main/java/com/chenxt/bootdemo/service/impl/AdminPermissionServiceImpl.java
@@ -0,0 +1,14 @@
+package com.chenxt.bootdemo.service.impl;
+
+import com.chenxt.bootdemo.service.IAdminPermissionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 后台管理权限service接口
+ *
+ * @author chenxt
+ * @date 2020/08/03
+ */
+@Service
+public class AdminPermissionServiceImpl implements IAdminPermissionService {
+}
